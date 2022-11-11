2dec96f35f70f4279b113c9255e1fd2f28bf292b
Enaz Banks <enazrbanks@gmail.com>
2022-11-11 12:57:27 +1100

    questions and answers

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,19 @@
 class QuestionsController < ApplicationController
+
   def ask
+  end
+
+  def answer
+    @answers = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+    if params[:question]
+      case params[:question]
+        when "I am going to work"
+         @answers = @answers[0]
+        when "?"
+          @answers =  @answers[1]
+        when ""
+          @answers = @answers[2]
+      end
+    end
   end
 end
